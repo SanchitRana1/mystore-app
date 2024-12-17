6b253b2658a26385b274a66c79475eb8bcd10b97
Rana <sanchitrana128@gmail.com>
2024-12-17 18:39:06 +0530

    next config fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript:{
-    ignoreBuildErrors: true
-  },
-  eslint:{
-    ignoreDuringBuilds:true``
-  },
-  experimental:{
-    serverActions: {
-      bodySizeLimit: "100MB"
-    }
-  },
-  images:{
-    remotePatterns:[{
-      protocol:'https',
-      hostname:'img.freepik.com'
-    },
-    {
-      protocol:'https',
-      hostname:'cloud.appwrite.io'
-    }]
-  }
+	typescript: {
+		ignoreBuildErrors: true
+	},
+	eslint: {
+		ignoreDuringBuilds: true
+	},
+	experimental: {
+		serverActions: {
+			bodySizeLimit: '100MB'
+		}
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'img.freepik.com'
+			},
+			{
+				protocol: 'https',
+				hostname: 'cloud.appwrite.io'
+			}
+		]
+	}
 };
 
 export default nextConfig;
